72db5c32a637d30239388a80b22fa4950167b397
lambrian <brianlam93@gmail.com>
2024-10-23 22:27:21 -0700

    add Berlin.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { Home } from './Home'
 import { Routes, Route } from 'react-router-dom'
 import { Queens } from './Queens'
 import { QueensBoard } from './QueensBoard'
-import { Photoset } from './PhotoSet'
+import { Berlin } from './Berlin'
 
 export const App = () => {
     return (
         <Routes>
             <Route path="" element={<Home />} />
-            <Route path="/photos/berlin" element={<Photoset />} />
+            <Route path="/photos/berlin" element={<Berlin />} />
             <Route path="/queens" element={<Queens />} />
             <Route path="/queens/:date" element={<QueensBoard />} />
         </Routes>
