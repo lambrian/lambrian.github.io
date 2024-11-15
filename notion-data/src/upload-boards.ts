e9b9775a8e777816a17ddc437e981250b44b207a
Brian Lam <brianlam93@gmail.com>
2024-11-15 09:40:14 -0300

    notion: split commands - add upload boards to notion (#89)

---
+++ notion-data/src/upload-boards.ts
@@ -0,0 +1,43 @@
+import { Client } from '@notionhq/client'
+import * as fs from 'fs'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+async function main() {
+    // read json of output
+    // write back to notion
+    const notion = new Client({ auth: process.env.NOTION_TOKEN })
+    const outputJson = fs.readFileSync('../queens-image-analysis/output.json', {
+        encoding: 'utf8',
+        flag: 'r',
+    })
+    const pageToBoard = JSON.parse(outputJson)
+    for (const page in pageToBoard) {
+        console.log(page, pageToBoard[page])
+
+        const pageId = page
+        const response = await notion.pages.update({
+            page_id: pageId,
+            properties: {
+                'Color Array': {
+                    rich_text: [
+                        {
+                            text: {
+                                content: JSON.stringify(pageToBoard[page]),
+                            },
+                        },
+                    ],
+                },
+            },
+        })
+        console.log(response)
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((err) => {
+        console.error(err)
+        process.exit(1)
+    })
