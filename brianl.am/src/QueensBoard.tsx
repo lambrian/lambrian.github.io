486031a1f464af060edcc7c55061e2f93f6a5af2
Brian Lam <brianlam93@gmail.com>
2024-11-15 21:03:35 -0300

    queens: queens from notion (#96)

--- brianl.am/src/QueensBoard.tsx
+++ brianl.am/src/QueensBoard.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useSearchParams } from 'react-router-dom'
 import { BOARDS } from './boards'
 import './queens.css'
 import { useCallback, useMemo, useState } from 'react'
+import BOARDS_NOTION from './board-data.json'
 declare module 'react' {
     interface CSSProperties {
         '--length'?: number
@@ -70,12 +71,17 @@ const Cell = (props: CellProps) => {
 
 export const QueensBoard = () => {
     const { date } = useParams()
+    const [searchParams] = useSearchParams()
+    let data = BOARDS
+    if (searchParams.get('useNotion')) {
+        data = BOARDS_NOTION
+    }
     const matchingBoard = useMemo(() => {
         if (!date) {
             return null
         }
-        return BOARDS.find((board) => board.date === date)
-    }, [date])
+        return data.find((board) => board.date === date)
+    }, [data, date])
 
     if (!matchingBoard) {
         return <div>Not Found</div>
