9d8efc3c805e0ba9ecd90b6b4f8a57e0ce49f060
Brian Lam <brianlam93@gmail.com>
2024-11-17 13:24:54 -0300

    queens: calendar view (#109)

--- brianl.am/src/Queens.tsx
+++ brianl.am/src/Queens.tsx
@@ -1,13 +1,77 @@
 import { Link } from 'react-router-dom'
 import BOARDS from './board-data.json'
+import { DateTime } from 'luxon'
 
 type QueenBoard = {
     date: string
     grid: number[]
 }
 
+const Calendar = ({ month, dates }: { month: string; dates: QueenBoard[] }) => {
+    const currMonth = DateTime.fromISO(month)
+    const daysInMonth = currMonth.daysInMonth
+    if (!currMonth || !daysInMonth) {
+        return <></>
+    }
+    const firstDayOfMonth = currMonth.startOf('month')
+    const weeksinMonth = Math.floor((firstDayOfMonth.weekday + daysInMonth) / 7)
+
+    // Get the first day of the current month
+    console.log(
+        'days, first day, weeks in month',
+        daysInMonth,
+        firstDayOfMonth.weekday,
+        weeksinMonth
+    )
+
+    const datesToRender = []
+    for (let i = 1; i < firstDayOfMonth.weekday; i++) {
+        datesToRender.push({ str: '' })
+    }
+    for (let i = 1; i <= daysInMonth; i++) {
+        const matchingBoard = dates.find(
+            (board) =>
+                board.date ===
+                firstDayOfMonth.plus({ days: i - 1 }).toFormat('yyyy-MM-dd')
+        )
+        datesToRender.push({ str: i, board: matchingBoard })
+    }
+    return (
+        <div className="calendar-wrapper">
+            <div className="calendar-title">
+                {DateTime.fromISO(month).toFormat('MMMM yyyy')}
+            </div>
+            <div className="calendar-view">
+                {datesToRender.map((date) => (
+                    <div className={`date ${date.board ? 'has-board' : ''}`}>
+                        {date.board ? (
+                            <Link to={`/queens/${date.board.date}`}>
+                                {date.str}
+                            </Link>
+                        ) : (
+                            date.str
+                        )}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
 const QUEEN_EMOJI = String.fromCodePoint(Number('128081'))
 export const Queens = () => {
+    const months: Map<string, QueenBoard[]> = new Map()
+    for (let i = 0; i < BOARDS.length; i++) {
+        const curr = BOARDS[i]
+        const month = DateTime.fromFormat(curr.date, 'yyyy-MM-dd').startOf(
+            'month'
+        )
+        const monthStr = month.toString()
+        const inMonth = months.get(monthStr) || []
+        inMonth.push(curr)
+        months.set(month.toString(), inMonth)
+    }
+    console.log('months', months)
     return (
         <div className="game-container">
             <div className="game-title">
@@ -16,10 +80,8 @@ export const Queens = () => {
                 <span className="decoration">{QUEEN_EMOJI}</span>
             </div>
             <div className="queens-list">
-                {BOARDS.map((board: QueenBoard) => (
-                    <div className="game-option">
-                        <Link to={`/queens/${board.date}`}>{board.date}</Link>
-                    </div>
+                {Array.from(months.entries()).map((curr) => (
+                    <Calendar month={curr[0]} dates={curr[1]} />
                 ))}
             </div>
         </div>
