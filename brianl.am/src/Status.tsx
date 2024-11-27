51da6e94ac16246294789445af885b0a85b39546
Brian Lam <brianlam93@gmail.com>
2024-11-27 20:17:47 -0300

    fix more status stuff

--- brianl.am/src/Status.tsx
+++ brianl.am/src/Status.tsx
@@ -1,5 +1,12 @@
 import './Status.css'
-import { useMemo, useState } from 'react'
+import {
+    Dispatch,
+    SetStateAction,
+    useCallback,
+    useMemo,
+    useRef,
+    useState,
+} from 'react'
 import statuses from './notion-data.json'
 
 const getChartIndicator = (location: string) => {
@@ -17,36 +24,6 @@ const getChartRel = (index: number, total: number) => {
     return `rel-${quarter}`
 }
 
-export const ChartIndicator = ({
-    index,
-    page,
-}: {
-    index: number
-    page: { location: string; date: string; rel: string }
-}) => {
-    const [isTooltipVisible, setTooltipVisible] = useState(false)
-    return (
-        <div
-            className={`chart-indicator ${getChartIndicator(page.location)} ${page.rel}`}
-            onMouseEnter={() => setTooltipVisible(true)}
-            onMouseLeave={() => setTooltipVisible(false)}
-            onFocus={() => setTooltipVisible(true)}
-            onBlur={() => setTooltipVisible(false)}
-            aria-describedby="tooltip"
-            style={{ position: 'relative' }}
-        >
-            {isTooltipVisible && (
-                <div className="chart-indicator-tooltip">
-                    <div className="triangle-bg"></div>
-                    <div className="triangle"></div>
-                    <div className="tooltip-date">{page.date}</div>
-                    <div className="tooltip-summary">{page.location}</div>
-                </div>
-            )}
-        </div>
-    )
-}
-
 interface Status {
     date: string
     location: string
@@ -59,6 +36,125 @@ const countTravelDays = (statuses: Array<Status>): string => {
     } / ${statuses.length}`
 }
 
+interface TooltipLocation {
+    top: number
+    left: number
+}
+export const ChartIndicator = ({
+    index,
+    page,
+    setTooltipLocation,
+    setTooltipVisible,
+    setCurrentStatusIndex,
+}: {
+    index: number
+    page: { location: string; date: string; rel: string }
+    setTooltipVisible: Dispatch<SetStateAction<boolean>>
+    setTooltipLocation: (newLocation: TooltipLocation) => void
+    setCurrentStatusIndex: Dispatch<SetStateAction<number>>
+}) => {
+    const ref = useRef<HTMLDivElement>(null)
+
+    const handleMouseEnter = () => {
+        if (ref.current) {
+            console.log('mouse enter')
+            setTooltipVisible(true)
+            const { top, left } = ref.current.getBoundingClientRect()
+            setTooltipLocation({ top, left })
+            setCurrentStatusIndex(index)
+        }
+    }
+    return (
+        <div
+            ref={ref}
+            className={`chart-indicator ${getChartIndicator(page.location)} ${page.rel}`}
+            onMouseEnter={handleMouseEnter}
+            aria-describedby="tooltip"
+            style={{ position: 'relative' }}
+        ></div>
+    )
+}
+
+const Chart = (props: { statuses: Array<Status> }) => {
+    const [isTooltipVisible, setTooltipVisible] = useState(false)
+    const [toolTipLocation, setTooltipLocation] = useState<TooltipLocation>({
+        top: 0,
+        left: 0,
+    })
+    const [currentStatusIndex, setCurrentStatusIndex] = useState(0)
+
+    const setTooltipLocationImpl = useCallback(
+        (newLocation: TooltipLocation) => {
+            setTooltipLocation(newLocation)
+        },
+        [setTooltipLocation]
+    )
+
+    return (
+        <div className="chart" onMouseLeave={() => setTooltipVisible(false)}>
+            {props.statuses.map((page, i) => (
+                <ChartIndicator
+                    index={i}
+                    page={page}
+                    setTooltipLocation={setTooltipLocationImpl}
+                    setTooltipVisible={setTooltipVisible}
+                    setCurrentStatusIndex={setCurrentStatusIndex}
+                />
+            ))}
+            {isTooltipVisible && (
+                <div
+                    className="chart-indicator-tooltip"
+                    style={{
+                        top: toolTipLocation.top,
+                        left: toolTipLocation.left,
+                    }}
+                >
+                    <div className="triangle-bg"></div>
+                    <div className="triangle"></div>
+                    <div className="tooltip-date">
+                        {statuses[currentStatusIndex].date}
+                    </div>
+                    <div className="tooltip-summary">
+                        {statuses[currentStatusIndex].location}
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+const LocationChart = (props: { statuses: Array<Status> }) => {
+    return (
+        <div className="section">
+            <div className="chart-section">
+                <div className="chart-title">Location</div>
+                <Chart statuses={props.statuses} />
+                <div className="chart-metrics">
+                    <div className="left-data">{props.statuses[0].date}</div>
+                    <div className="spacer"></div>
+                    <div className="center-data">{`${countTravelDays(props.statuses)} days out of San Francisco`}</div>
+                    <div className="spacer"></div>
+                    <div className="right-data">
+                        {props.statuses[statuses.length - 1].date}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const IncidentList = (props: { statuses: Array<Status> }) => {
+    return (
+        <div className="section">
+            <div className="section-title">Previous Incidents</div>
+            {statuses.toReversed().map((status) => (
+                <div className="summary-day-section">
+                    <div className="summary-date">{status.date}</div>
+                    <div className="summary-detail">{status.location}</div>
+                </div>
+            ))}
+        </div>
+    )
+}
 export const StatusPage = () => {
     const displayedStatuses = useMemo(() => {
         return statuses.map((page, i) => ({
@@ -72,46 +168,8 @@ export const StatusPage = () => {
         <div className="page-wrapper">
             <div className="page">
                 <div className="page-title">Status History</div>
-                <div className="section">
-                    <div className="chart-section">
-                        <div className="chart-title">Location</div>
-                        <div className="chart">
-                            {displayedStatuses.map((page, i) => (
-                                <ChartIndicator index={i} page={page} />
-                            ))}
-                        </div>
-                        <div className="chart-metrics">
-                            <div className="metric-rel-3">
-                                {
-                                    displayedStatuses.find(
-                                        (day) => day.rel === 'rel-3'
-                                    )?.date
-                                }
-                            </div>
-                            <div className="spacer"></div>
-                            <div>{`${countTravelDays(displayedStatuses)} days out of San Francisco`}</div>
-                            <div className="spacer"></div>
-                            <div>
-                                {
-                                    displayedStatuses[
-                                        displayedStatuses.length - 1
-                                    ].date
-                                }
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="section">
-                    <div className="section-title">Previous Incidents</div>
-                    {displayedStatuses.reverse().map((page) => (
-                        <div className="summary-day-section">
-                            <div className="summary-date">{page.date}</div>
-                            <div className="summary-detail">
-                                {page.location}
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                <LocationChart statuses={displayedStatuses} />
+                <IncidentList statuses={displayedStatuses} />
             </div>
         </div>
     )
