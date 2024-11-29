e51d40c916a328d3eb969ea7bd2570300f852be8
Brian Lam <brianlam93@gmail.com>
2024-11-29 07:28:47 -0300

    home: changed home a little (#124)

--- brianl.am/src/Home.tsx
+++ brianl.am/src/Home.tsx
@@ -4,21 +4,20 @@ import { Link } from 'react-router-dom'
 export const Home = () => {
     return (
         <div className="content-wrapper">
-            <div className="intro">Hey there, I'm Brian 👋</div>
+            <div className="intro">
+                Hey there, I'm Brian <div className="handwave">👋</div>
+            </div>
             <div className="intro-description">
                 <p>
-                    I'm an engineering manager of the team at Figma thinking
-                    about how design systems supercharges designers and design
-                    teams.
+                    I'm an engineering manager at Figma working on design
+                    systems and more broadly how designs are shared and reused.
                 </p>
                 <p>
-                    I was previously an engineer, most recently at Figma, and
-                    previously at Meta building various parts of Instagram,
-                    Messenger, and Ads. Before that, I was at Stanford
-                    University where I studied Computer Science.
+                    I was previously an engineer here at Figma and at Instagram,
+                    and Facebook Messenger. Before that, I studied Computer
+                    Science at Stanford.
                 </p>
                 <p>
-                    {' '}
                     I like taking photos which I sometimes share on my{' '}
                     <a href="https://instagram.com/_brianlam">Instagram</a>. I
                     most recently traveled to{' '}
