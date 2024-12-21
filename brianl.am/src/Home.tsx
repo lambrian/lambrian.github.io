2a4d5d84639f9f7da739724a8d4aa662d621c896
lambrian <brianlam93@gmail.com>
2024-12-20 23:00:58 -0800

    change home page

--- brianl.am/src/Home.tsx
+++ brianl.am/src/Home.tsx
@@ -9,19 +9,16 @@ export const Home = () => {
             </div>
             <div className="intro-description">
                 <p>
-                    I'm an engineering manager at Figma working on design
-                    systems and more broadly how designs are shared and reused.
-                </p>
-                <p>
-                    I was previously an engineer here at Figma and at Instagram,
-                    and Facebook Messenger. Before that, I studied Computer
+                    I'm an engineering manager at Figma and previously an
+                    engineer here and elsewhere. Before that, I studied Computer
                     Science at Stanford.
                 </p>
                 <p>
                     I like taking photos which I sometimes share on my{' '}
-                    <a href="https://instagram.com/_brianlam">Instagram</a>. I
-                    most recently traveled to{' '}
-                    <Link to="/photos/berlin">Berlin</Link>.
+                    <a href="https://instagram.com/_brianlam">Instagram</a>.
+                    Some places I've been are
+                    <Link to="/photos/berlin"> Berlin</Link> and{' '}
+                    <Link to="/photos/south-america">South America</Link>.
                 </p>
             </div>
         </div>
