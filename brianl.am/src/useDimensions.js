f00182e4c817e164b72efbb3c25202fa74349223
Brian Lam <brianlam93@gmail.com>
2024-12-26 21:28:32 -0600

    viewport animation

--- brianl.am/src/useDimensions.js
+++ brianl.am/src/useDimensions.js
@@ -10,11 +10,11 @@ const subscribe = (callback) => {
 }
 
 export const useDimensions = (ref) => {
-    const dimensions = useSyncExternalStore(subscribe, () =>
-        JSON.stringify({
-            width: ref.current?.offsetWidth ?? 0, // 0 is default width
-            height: ref.current?.offsetHeight ?? 0, // 0 is default height
+    const dimensions = useSyncExternalStore(subscribe, () => {
+        return JSON.stringify({
+            width: ref?.offsetWidth ?? 0, // 0 is default width
+            height: ref?.offsetHeight ?? 0, // 0 is default height
         })
-    )
+    })
     return useMemo(() => JSON.parse(dimensions), [dimensions])
 }
